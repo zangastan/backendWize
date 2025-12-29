913fe00684b07670bcf6be3f43cd785123bb6e16
Augustine Kasolota <kasolotaaugustine@gmail.com>
2025-12-29 21:33:25 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,21 +5,18 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(title="Personal Assistant for Augustine Kasolota ")
 
 
-# Allow requests from your frontend origin (Next.js dev server)
 origins = [
-    "http://localhost:3000",  # 
-    "http://127.0.0.1:5500", # testing...
-    "https://augustinekasolota.netlify.app/"
+    "http://localhost:3000",
+    "https://augustinekasolota.netlify.app"
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],   # allow GET, POST, OPTIONS, etc.
+    allow_credentials=False,  # IMPORTANT
+    allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(chat_router.router)
 
